74d79577b6ad3fdeabeb9fa277eef2cb32aaad6e
Alan Berns <52006716+alanberns@users.noreply.github.com>
2022-05-24 16:57:52 -0300

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
 /* GET home page. */
 router.all('/', function(req, res, next) {
   res.render('index', { title: 'Inicio | VacunAssist' });
@@ -17,6 +21,57 @@ router.post('/registro', function(req, res, next) {
 
 router.post('/registroPasoDos', function(req, res, next) {
   res.render('registroPasoDos', { title: 'Registro | VacunAssist' });
+});
+
+router.get('/misDatos', async function(req, res, next){
+	const paciente = await prisma.paciente.findUnique({
+		where:{
+			id:2 //obtener id o algo desde el log de sesion
+		}
+	})
+
+	res.render('misDatos', { title: 'Mis datos | VacunAssist', paciente: paciente });
+});
+
+
+router.get('/misDatosModificar', async function(req, res, next){
+	const paciente = await prisma.paciente.findUnique({
+		where:{
+			id:2 //obtener id o algo desde el log de sesion
+		}
+	})
+	res.render('misDatosModificar', { title: 'Modificar mis datos | VacunAssist', paciente: paciente });
+});
+
+router.post('/misDatosModificar', function(req, res, next){
+	var paciente = {
+		email: req.body.email ,
+		nombre: req.body.nombre ,
+		apellido: req.body.apellido ,
+		fechaNacimiento: req.body.fechaNacimiento ,
+		dni: req.body.dni ,
+		genero: req.body.genero ,
+		centro: req.body.centro ,
+		esRiesgo: req.body.esRiesgo ,
+	}
+	res.render('misDatos', { title: 'Mis datos | VacunAssist', paciente: paciente }); 
+});
+
+
+router.post('/modificarMail', function(req, res, next){
+	res.render('modificarMail', {title: 'Cambiar mail | VacunAssist'});
+});
+
+router.get('/modificarMail', function(req, res, next){
+	res.render('modificarMail', {title: 'Cambiar mail | VacunAssist'});
+});
+
+router.get('/modificarContrasenia', function(req, res, next){
+	res.render('modificarContrasenia', {title: 'Cambiar contraseña | VacunAssist'});
+});
+
+router.post('/modificarContrasenia', function(req, res, next){
+	res.render('modificarContrasenia', {title: 'Cambiar contraseña | VacunAssist'});
 });
 
 module.exports = router;
